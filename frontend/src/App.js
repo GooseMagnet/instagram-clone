da64002efc8ecb0e028a99575318f439364174dc
Ilan Karasik <ilan.karasik@centro.net>
2022-04-12 01:49:39 -0400

    Finish mobile profile view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Profile from "./components/Profile";
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => (
+  <Routes>
+    {/* Logged in Routes */}
+    <Route path="/" element={<LoggedIn />}>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/:username" element={<Profile />} />
+    </Route>
+
+    {/* Logged out Routes */}
+    <Route path="/password/reset" element={<PasswordReset />} />
+    <Route path="/login" element={<Login />} />
+  </Routes>
+);
+
+const Home = () => {
+  // Fetch photos from all this user follows, sorted chronologically
+  return <h1>Home</h1>;
+};
+
+const LoggedIn = () => (
+  <>
+    <Outlet />
+    <Footer />
+  </>
+);
+
+const Login = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="login">
+      <img src="wordmark.png" alt="Instagram" />
+      <form className="login__form">
+        <input type="text" placeholder="Phone number, username, or email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Log In</button>
+        <div className="login__or" style={{ display: "flex" }}>
+          <div>―――――――</div>
+          <div style={{ padding: "4px 8px 4px 8px" }}>OR</div>
+          <div>―――――――</div>
+        </div>
+        <button className="login__facebook">Log in with Facebook</button>
+        <Link to={"/password/reset"}>Forgot password?</Link>
+      </form>
+      <div className="signup">
+        <p>Don't have an account?</p>
+        <Link to={"/signup"}>Sign up</Link>
+      </div>
+      <div className="download">
+        <p>Get the app.</p>
+        <img src="/download-appstore.png" alt="Download on the App Store" />
+        <img src="/download-googleplay.png" alt="Get it on Google Play" />
+      </div>
+    </section>
   );
-}
+};
+
+const PasswordReset = () => <h1>Reset Password</h1>;
 
 export default App;
